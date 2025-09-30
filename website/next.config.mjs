2ac3f7e07530ddcacc3b1575a8b36f7f73f2b279
bencahoon01 <160652034+bencahoon01@users.noreply.github.com>
2025-09-29 23:37:56 -0400

    added trailing slash

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  
+  // Enforce a trailing slash on all URLs (e.g., /about -> /about/)
+  trailingSlash: true,
+  
+  // Set the path prefix for GitHub Pages
   basePath: '/portfolio',
   assetPrefix: '/portfolio/',
-  
+
   eslint: {
     ignoreDuringBuilds: true,
   },
